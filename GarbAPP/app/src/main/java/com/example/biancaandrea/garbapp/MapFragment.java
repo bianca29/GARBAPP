a654e946ab45ebd078f251fad14c89c421ac2086
Manuel <manu231096@hotmail.com>
2018-11-15 19:57:48 -0500

    Nueva version 2

--- GarbAPP/app/src/main/java/com/example/biancaandrea/garbapp/MapFragment.java
+++ GarbAPP/app/src/main/java/com/example/biancaandrea/garbapp/MapFragment.java
@@ -2,16 +2,57 @@ package com.example.biancaandrea.garbapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
-public class MapFragment extends Fragment {
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
+    private MapView mapView;
+    private GoogleMap gmap;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View main_view = inflater.inflate(R.layout.fragment_map, container, false);
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        }
+
+        mapView = main_view.findViewById(R.id.map_view);
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);
+
+        return main_view;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gmap = googleMap;
+        gmap.setMinZoomPreference(12);
+        LatLng ny = new LatLng(-16.393149, -71.550259);
+        gmap.moveCamera(CameraUpdateFactory.newLatLng(ny));
     }
 }
